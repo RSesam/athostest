d2db1addfc0555eba0dfb273415eb08ee4d922a2
Rene Schweiger <rschweiger@gmx.de>
2012-07-26 11:22:25 +0200

    Fuer ATHOS eine Moelichkeit schaffen (ZetaTest)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athostest::Application.config.secret_token = '15172dbbc4f8bda7b08f62a5d5cf0595f4162abd81c5b0e27b1f55e60b0853f26ec4fb100f327463a7c3c5808b08fb3cbdc44abe6bcdb1ca95f4e8cf476ee4ee'
